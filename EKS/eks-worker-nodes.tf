f21c224ca746f108faa78fbaa37e16d709bff63d
ayoobnazzz <59804587+ayoobnazzz@users.noreply.github.com>
2020-07-18 16:49:52 -0500

    Update eks-worker-nodes.tf

--- EKS/eks-worker-nodes.tf
+++ EKS/eks-worker-nodes.tf
@@ -105,9 +105,9 @@ resource "aws_eks_node_group" "demo" {
 
 
   scaling_config {
-    desired_size = 2
-    max_size     = 4
-    min_size     = 2
+    desired_size = 1
+    max_size     = 10
+    min_size     = 1
   }
 
   depends_on = [
